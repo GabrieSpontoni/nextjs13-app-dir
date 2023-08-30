a62b42bd5b16cfecfb3c8f0cb876db014eda78bc
Gabriel Spontoni <gabriel.spontoni@mblabs.com.br>
2023-08-30 09:55:26 -0400

    fix: hidden menu hamburguer when sidebar is not available

--- src/app/[locale]/Providers.tsx
+++ src/app/[locale]/Providers.tsx
@@ -3,11 +3,11 @@
 import { Header } from "@/components/Header";
 import { Sidebar } from "@/components/Sidebar";
 import { RenderCondition } from "@/utils/RenderCondition";
+import { urlsWithoutSidebar } from "@/utils/sidebar";
 import { ThemeProvider } from "next-themes";
 import { usePathname } from "next/navigation";
 
 export function Providers({ children }: React.PropsWithChildren<{}>) {
-  const urlsWithoutSidebar = ["/"];
   const pathname = usePathname();
   const showSidebar = !!pathname && !urlsWithoutSidebar.includes(pathname);
   return (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,24 @@ import { ThemeSwitcher } from "./components/ThemeSwitcher/ThemeSwitcher";
 import { LanguageSwitcher } from "./components/LanguageSwitcher/LanguageSwitcher";
 import { Links } from "./components/Links/Links";
 import { MdMenu } from "react-icons/md";
+import { RenderCondition } from "@/utils/RenderCondition";
+import { usePathname } from "next/navigation";
+import { urlsWithoutSidebar } from "@/utils/sidebar";
 
 export function Header() {
+  const pathname = usePathname();
+  const showMenuHamburguer =
+    !!pathname && !urlsWithoutSidebar.includes(pathname);
   return (
     <div className="flex bg-base-100 fixed z-10 w-full">
-      <label
-        htmlFor="my-drawer-2"
-        className="btn my-2 lg:hidden bg-transparent border-none"
-      >
-        <MdMenu size={24} />
-      </label>
+      <RenderCondition condition={showMenuHamburguer}>
+        <label
+          htmlFor="my-drawer-2"
+          className="btn my-2 lg:hidden bg-transparent border-none"
+        >
+          <MdMenu size={24} />
+        </label>
+      </RenderCondition>
       <div className="navbar px-4 lg:px-10">
         <div className="navbar-start">
           <Links />
