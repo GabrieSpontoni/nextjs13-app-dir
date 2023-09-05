9fdb2696f4ac6196650fdf588625a7948be8f7d7
Gabriel Spontoni <gabriel.spontoni@mblabs.com.br>
2023-09-05 13:11:36 -0400

    fix: sidebar active

--- src/components/Sidebar/components/SidebarList/SidebarList.tsx
+++ src/components/Sidebar/components/SidebarList/SidebarList.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useTranslations } from "next-intl";
-import { usePathname } from "next/navigation";
+import { useTranslations, useLocale } from "next-intl";
+import { usePathname } from "next-intl/client";
 
 export function SidebarList() {
   const pathname = usePathname();
+  const locale = useLocale();
   const t = useTranslations("Tutorials");
   const sidebarItems = [
     {
@@ -21,17 +22,13 @@ export function SidebarList() {
   return (
     <ul className="menu py-10 w-80 min-h-full bg-base-100 text-base-content">
       {sidebarItems.map((item, index) => (
-        <li
-          key={index}
-          className={`${
-            item.disabled
-              ? "disabled"
-              : `${
-                  pathname === item.url ? "bg-base-300 rounded-lg" : undefined
-                }`
-          }`}
-        >
-          <a href={item.disabled ? undefined : item.url}>{item.title}</a>
+        <li key={index} className={`${item.disabled ? "disabled" : undefined}`}>
+          <a
+            className={`${pathname === item.url ? "active" : undefined}`}
+            href={item.disabled ? undefined : `/${locale}${item.url}`}
+          >
+            {item.title}
+          </a>
         </li>
       ))}
     </ul>
